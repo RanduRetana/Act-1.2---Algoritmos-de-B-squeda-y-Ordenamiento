b24b73dfc9e4dedc84862568bdfcbd9b4143b06a
RanduRetana <106175823+RanduRetana@users.noreply.github.com>
2022-09-06 23:40:53 -0500

    Update main.cpp

--- tc1031-act12-search-sorting-algorithms-template/main.cpp
+++ tc1031-act12-search-sorting-algorithms-template/main.cpp
@@ -20,17 +20,15 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	vector<int> v1{45, 35 , 69, 12, 6};
-	struct timeval start, end;
-	gettimeofday(&start, NULL);
-	ios_base::sync_with_stdio(false);
+	clock_t start, end;
+	start = clock();
 	bubbleSort(v1);
 	binarySearch(v1,69);
-	gettimeofday(&end, NULL);
-    double time_taken;
-    time_taken = (end.tv_sec - start.tv_sec) * 1e6;
-    time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
-    cout << "Time taken by program is : " << fixed<< time_taken << setprecision(6);
-    cout << " sec" << endl;
+	end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout<<end <<endl <<start;
+    cout << "Time taken by program is : " << fixed << time_taken << setprecision(5);
+    cout << " sec " << endl;
 	//insertionSort(v1);
 	//selectionSort(v1);
 	//cout<<v1[0];
